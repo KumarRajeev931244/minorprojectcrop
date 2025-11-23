ab559510a59dd0b294f630b0ca956866e0fe83f6
rajeev <rajeevkumar25112002@gmail.com>
2025-11-23 20:15:21 +0530

    feat: add user verification process with email notifications
    - Added `@react-email/components` and `resend` dependencies for email handling.
    - Implemented `VerificationEmailTemplates` for crafting verification emails.
    - Created `sendVerificationEmail` helper function to send verification emails using Resend API.
    - Updated `dbConnect` to include database name configuration.
    - Developed signup API route to handle user registration and send verification emails.
    - Introduced `ApiResponse` type for consistent API response structure.
    - Added `constant.ts` for storing database name.

--- crop/src/lib/dbConnect.ts
+++ crop/src/lib/dbConnect.ts
@@ -1,17 +1,22 @@
 import mongoose from "mongoose";
+import { dbName } from "@/constant/constant";
 
 type ConnectionObject = {
     isConnected?: number
 }
 const connection: ConnectionObject = {}
 async function dbConnect():Promise<void>  {
+    console.log("connection is connected:",connection);
     if(connection.isConnected){
         console.log("db connection already established")
         return
     }
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || '')
+        const db = await mongoose.connect(process.env.MONGODB_URI || '',{
+            dbName: dbName
+        })
         console.log("connection:",db);
+        console.log(`Connected to database: ${db.connections[0].name}`);
         connection.isConnected= db.connections[0].readyState        
        
     } catch (error) {
